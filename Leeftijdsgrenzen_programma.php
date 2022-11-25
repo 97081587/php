e7f43e88edc2a2e70184258a9e2763ecb4115a17
Oscar <97081587@st.deltion.nl>
2022-11-25 14:26:37 +0100

    Create Leeftijdsgrenzen_programma.php

---
+++ Leeftijdsgrenzen_programma.php
@@ -0,0 +1,27 @@
+<?php
+//geef leeftijd
+$invoer = readline("geef leeftijd: ");
+
+//stop
+if ($invoer < 9 || $invoer > 118) {
+    exit("te oud/te jong");
+}
+
+// felicitaties + waarschuwing
+while($invoer = 10 || $invoer = 16 || $invoer = 18|| $invoer = 50 || $invoer = 67 || $invoer = 100) {
+    if ($invoer = 62) {
+        echo "het duurt nog 5 jaar voordat je 67 bent.";
+    }
+    echo "gefeliciteerd!";
+}
+
+
+
+//felicitaties
+//if ($invoer = 10 || $invoer = 16 || $invoer = 18|| $invoer = 50 || $invoer = 67 || $invoer = 100)
+
+//waarschuwing
+//while($invoer != 62) {
+    
+
+?>
