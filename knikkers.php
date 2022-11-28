7252ddbbe3fc349b0c3563783317ed9959535b55
Oscar <97081587@st.deltion.nl>
2022-11-28 12:32:08 +0100

    bijna

---
+++ knikkers.php
@@ -0,0 +1,18 @@
+<?php
+// geef knikkers
+$invoer = readline("geef aantal knikkers die in de zak zitten: ");
+
+//limiet
+if ($invoer < 1 || $invoer > 20) {
+    exit("je moet kiezen tussen 1 en 20 knikkers");
+}
+
+//knikker teller
+$begin=1;
+$maximum=$invoer;
+
+for ($teller=$begin;$teller<=$maximum;$teller++) {
+    $begin-$invoer;
+    echo $begin." knikkers opgegooid, nog ".$invoer." in mijn zak\n";
+}
+?>
