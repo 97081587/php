1a1e6cc21d684a29d7d4afc2b49285729624677a
Oscar <97081587@st.deltion.nl>
2023-03-17 10:23:35 +0100

    pls

--- super pizza opdracht.php
+++ super pizza opdracht.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="nl">
+    
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pizza di mama homepage 🍕🍕🍕</title>
+    <title>Home of the world's most delicious pizza 🍕🍕🍕</title>
     <link rel="icon" href="pizza.png">
     <link href="pizza.css" rel="stylesheet">
 </head>
+
 <body>
     
 </body>
