616e7afd82f72497fdd3d276b2ece8aec2454b52
Oscar <97081587@st.deltion.nl>
2023-03-17 09:22:36 +0100

    pizza

--- super pizza opdracht.php
+++ super pizza opdracht.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pizza di mama homepage</title>
+    <title>Pizza di mama homepage 🍕🍕</title>
+    <link rel="icon" href="pizza.png">
 </head>
 <body>
     
