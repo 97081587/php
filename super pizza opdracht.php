3d11edd2cc1b24698081780872598d7975fde6c1
Oscar <97081587@st.deltion.nl>
2023-04-04 10:35:27 +0200

    ruimte

--- super pizza opdracht.php
+++ super pizza opdracht.php
@@ -11,51 +11,51 @@
 
 <body>
 <p>pizza di mama</p>
- <form method=post action="">
-  <div class="fname">
-    <label for="fname">Voornaam:</label>
-    <br>
-    <input type="text" id="fname" name="fname"
-      placeholder="Voornaam" value="" required>
-    <br>
-  </div>
-   <div class="adres">
-     <label for="adres">Adres:</label>
+  <form method=post action="">
+   <div class="fname">
+     <label for="fname">Voornaam:</label>
      <br>
-     <input type="text" id="adres" name="adres"
-       placeholder="Adres" value="" required>
+     <input type="text" id="fname" name="fname"
+       placeholder="Voornaam" value="" required>
      <br>
    </div>
-    <div class="postcode">
-       <label for="Pcode">Postcode:</label>
-       <br>
-       <input type="text" id="pcode" name="pcode"
-         placeholder="Postcode" maxlength="12" value="" required>
-       <br>
+    <div class="adres">
+      <label for="adres">Adres:</label>
+      <br>
+      <input type="text" id="adres" name="adres"
+        placeholder="Adres" value="" required>
+      <br>
     </div>
-   <div class="plaats">
-     <label for="Plaats">Plaats:</label>
-     <br>
-     <input type="text" id="plaats" name="plaats"
-       placeholder="Plaats" maxlength="85" value="" required>
-     <br>
+     <div class="postcode">
+        <label for="Pcode">Postcode:</label>
+        <br>
+        <input type="text" id="pcode" name="pcode"
+          placeholder="Postcode" maxlength="12" value="" required>
+        <br>
+     </div>
+    <div class="plaats">
+      <label for="Plaats">Plaats:</label>
+      <br>
+      <input type="text" id="plaats" name="plaats"
+        placeholder="Plaats" maxlength="85" value="" required>
+      <br>
+    </div>
+   <div class="Bdatum">
+      <label for="Bdatum">Besteldatum:</label>
+      <br>
+      <input type="date" id="Bdatum" name="Bdatum" value="2022-03-15" min="2022-03-15" max="2022-04-24">
    </div>
-  <div class="Bdatum">
-     <label for="Bdatum">Besteldatum:</label>
-     <br>
-     <input type="date" id="Bdatum" name="Bdatum" value="2022-03-15" min="2022-03-15" max="2022-04-24">
-  </div>
-<div class="BeOfAf">
-  <label for="BeOfAf">bezorgen of afhalen?</label>
-  <br>
-  <label for="BeOfAf">bezorgen</label>
-  <input type="radio" id="BeOfAf" name="BeOfAf"
-    placeholder="BeOfAf" value="">
-  <label for="BeOfAf">afhalen</label>
-  <input type="radio" id="BeOfAf" name="BeOfAf"
-    placeholder="BeOfAf" value="">
-  <br>
-</div>
+ <div class="BeOfAf">
+   <label for="BeOfAf">bezorgen of afhalen?</label>
+   <br>
+   <label for="BeOfAf">bezorgen</label>
+   <input type="radio" id="BeOfAf" name="BeOfAf"
+     placeholder="BeOfAf" value="">
+   <label for="BeOfAf">afhalen</label>
+   <input type="radio" id="BeOfAf" name="BeOfAf"
+     placeholder="BeOfAf" value="">
+   <br>
+ </div>
 <input type="submit" name="submit" value="gegevens versturen" />
 </form>  
 </body>
