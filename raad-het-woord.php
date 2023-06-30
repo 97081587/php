c61e60fe2abfcfe81774f35f6c71706b05cacf2b
Oscar <97081587@st.deltion.nl>
2023-06-30 13:59:03 +0200

    opslaan2

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -1,10 +1,14 @@
 <?php
+$letter = '';
 $rndmwoord = array("peel","bmw");
 
-echo 'Het woord is: ';
-$woord = readline('Kies een letter: \n');
-
-
-
-
+while (stripos($rndmwoord, $lword)) {
+    echo 'Het woord is: ';
+    $letter = readline('Kies een letter: \n');
+if (stripos($rndmwoord, $letter) ==false) {
+      echo "letter $letter komt niet voor in het woord.";
+    } else {
+        echo "Het woord is: $letter ";
+    }
+}   
 ?>
