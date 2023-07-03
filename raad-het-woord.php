bccaccd8b8fd7c457ea29783c27af5e9478f8972
Oscar <97081587@st.deltion.nl>
2023-07-03 12:02:24 +0200

    o

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -1,18 +1,19 @@
 <?php
-$letter = readline('');
-$rij = array("peer","appel", "banaan");
-$rndmwoord =array_rand($rij);
+$letter ='';
+$rij = array("peer", "appel", "banaan");
+$rndmwoord =array_rand($rij, 2);
 
 $teller=1;
 
-
-    echo 'Het woord is: ';
-    $letter = readline('Kies een letter: \n');
-        if (stripos($rndmwoord, $letter) ==false) {
-            echo "letter $letter komt niet voor in het woord.";
-        } else {
-             $teller++;
-            echo "Het woord is: $rndmwoord, je hebt het graden in $teller pogingen.";
-        } 
- 
+while ($letter !== $rij) {
+    echo "Het woord is: \n";
+    $letter = readline("Kies een letter: \n");
+        if (stripos($rij[$rndmwoord[1]], $letter) ==false) {
+            echo "letter $letter komt niet voor in het woord.\n";
+        }
+        $teller++;
+    }
+    echo "Het woord is: ";
+    echo $rij[$rndmwoord[2]];
+    echo "je hebt het geraden in $teller pogingen.";
 ?>
