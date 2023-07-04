95252d9b89f90dc74caabb072d96e2bef44c958b
Oscar <97081587@st.deltion.nl>
2023-07-04 08:48:07 +0200

    nog alleen dat letter laten zien

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -5,14 +5,16 @@ $rndmwoord =array_rand($rij, 2);
 
 $teller=1;
 
-while ($letter !== $rndmwoord) {
-    echo "Het woord is: \n";
+echo "Het woord is: \n";
+while ($letter !== $rndmwoord) {   
     $letter = readline("Kies een letter: \n");
-        if (stripos($rij[$rndmwoord[1]], $letter) ==false) {
+     if (stripos($rij[$rndmwoord[1]], $letter) ==false) {
             echo "letter $letter komt niet voor in het woord.\n";
+        } else {
+         echo "Het woord is: \n";
         }
-        $teller++;
-    }
+    $teller++;
+}
     echo "Het woord is: ";
     echo $rij[$rndmwoord[2]];
     echo "je hebt het geraden in $teller pogingen.";
