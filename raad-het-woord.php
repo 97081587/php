b38df0f2923d9228785a49e6a1dfa4b5400f1540
Oscar <97081587@st.deltion.nl>
2023-07-05 14:20:18 +0200

    uhh want moet  ik er in doen

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -1,21 +1,20 @@
 <?php
-$letter ='';
-$rij = array("appel", "banaan", "tos");
-$rndmwoord =array_rand($rij, 3);
+$array = array("appel", "banaan", "tos");
+$rndmwoord = array_rand($array, 2);
 
 $teller=1;
 
-echo "Het woord is: \n";
-while ($letter !== $rij[$rndmwoord[0]]) {   
-    $letter = readline("Kies een letter: \n");
-     if (stripos($rij[$rndmwoord[0]], $letter) ==false) {
-            echo "Het woord is: \n";
-        } else {
-          echo "letter $letter komt niet voor in het woord.\n";
+echo "Het woord is:  \n";
+  while ( ) {
+      $letter = readline("Kies een letter: \n");
+       if ( ) {
+            echo "letter $letter komt niet voor in het woord.\n";
+          } else {
+            echo "Het woord is:  \n";
         }
     $teller++;
-}
-echo "Het woord is:" .$rij[$rndmwoord[0]],", je hebt het geraden in $teller pogingen.\n";
+  }
+echo "Het woord is:" .[$array[$rndmwoord[1]]],", je hebt het geraden in $teller pogingen.\n";
 echo "Wil je nog een keer spelen (j/n)";
 if ($letter == "n"){  exit();
 }
