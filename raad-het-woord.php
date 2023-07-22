8d2fba67d7c07e0ba2d447b6db792534569ef230
Oscar <97081587@st.deltion.nl>
2023-07-22 12:36:13 +0200

    letter split

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -1,19 +1,22 @@
 <?php
-$array = array("appel", "banaan", "tos");
-$rndmwoord = array_rand($array);
+$banaan = array("b", "a", "n", "a", "a", "n");
+$appel = array("a", "p", "p", "e", "l");
+$tos = array("t", "o", "s");
+$megaarray = array($appel, $banaan, $tos);
+$rndmwoord = array_rand($megaarray, 1);
 
 $teller=0;
 //moet nog letter detectie en woord in asterisks.
 echo "Het woord is: \n";
 $letter = readline("Kies een letter: ");
-  while (// deze moet gaan loopen tot de hele woord is geraden) {
+  //while ($letter != $rndmwoord) {
        if (str_contains($rndmwoord, $letter)) {
             echo "Het woord is: \n";
           } else {
             echo "letter $letter komt niet voor in het woord.\n";
         }
       $teller++;
-    };
+    //};
 
 echo "Het woord is: $rndmwoord, je hebt het geraden in $teller pogingen.\n";
 echo "Wil je nog een keer spelen (ja/nee)";
