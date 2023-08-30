f2f2fbbfe9f203c6e05e95268655cf4f465a2f5b
Oscar <97081587@st.deltion.nl>
2023-08-30 16:06:25 +0200

    plan

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -1,15 +1,20 @@
 <?php
-$banaan = array("b"=>"1", "a"=>"2", "n"=>"3", "a"=>"4", "a"=>"5", "n"=>"6");
-$appel = array("a"=>"1", "p"=>"2", "p"=>"3", "e"=>"4", "l"=>"5");
-$tos = array("t"=>"1", "o"=>"3", "s"=>"3");
-$megaarray = array($appel, $banaan, $tos);
-$rndmwoord = array_rand($megaarray, 1);// array in array werkt niet haha
+$banaan = "banaan";
+$appel = "appel";
+$zit = "zit";
+$megaarray = array($appel, $banaan, $zit);
+$rndmwoord = array_rand($megaarray, 1);
 
 $teller=0;
-//moet nog letter detectie en woord in asterisks.
-echo "Het woord is: \n";
+//hier letters tellen van woord en dan in asterisks zetten
+echo strlen($rndmwoord);
+ if ($rndmwoord > 0) {
+  echo str_replace($rndmwoord, "*", $rndmwoord);
+ }
+echo "het woord is: \n";
+//hier dan de loop voor elk letter van de woord
 $letter = readline("Kies een letter: ");
-  while ($letter != $megaarray) { // loop is ook beetje kapot
+  while ($letter != $megaarray) {
        if (array_search("1", $megaarray, true)) {
             echo "Het woord is: \n";
           }
@@ -22,7 +27,7 @@ $letter = readline("Kies een letter: ");
 echo "Het woord is: $rndmwoord, je hebt het geraden in $teller pogingen.\n";
 echo "Wil je nog een keer spelen (ja/nee)";
 if ($letter == "ja") {
-  header("Refresh:0");
+
 }
 if ($letter == "nee") {  
   exit("zie je de volgende keer!");   
