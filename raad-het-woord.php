f6e2757e79bdaf67d77ac56c649ebbd3fe2a7dae
Oscar <97081587@st.deltion.nl>
2023-09-11 12:23:21 +0200

    loop

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -1,5 +1,5 @@
 <?php
-$array = array("appel","banaan","fruit");
+$array = array("leerling","cirkel","eend","veiligheidsspeld","taart","arend","verkoper","dier","goed","ballonnen","borstel","pen","koffie","knie","kast");
 $rndmwoord = array_rand($array, 1);
 
 echo "Het woord is: ";
@@ -8,11 +8,11 @@ $lengte = strlen ($array[$rndmwoord]);
 for ($i=0;$i<=$lengte;$i++) {
   echo "*";
 }
-$raadwoord = "*****";
+$raadwoord = $rndmwoord;
 $teller=1;
 $begin=1;
 //hier dan de loop voor elk letter van de woord
-$letter = readline("Kies een letter: ");
+$letter = readline(" Kies een letter: ");
   while ($rndmwoord != $raadwoord) {
        if (($letter == $array[$rndmwoord])) {
              echo "Het woord is: \n";
@@ -22,12 +22,11 @@ $letter = readline("Kies een letter: ");
       $teller++;
     };
 
-echo "Het woord is: $rndmwoord, je hebt het geraden in $teller pogingen.\n";
+echo "Het woord is: " .($array[$rndmwoord]); echo ", je hebt het geraden in $teller pogingen.\n";
 echo "Wil je nog een keer spelen (ja/nee)";
 if ($letter == "ja") {
    echo "ja";
 } elseif ($letter == "nee") {  
    exit("zie je de volgende keer!");   
 }
-echo "DEBUG: done";
 ?>
