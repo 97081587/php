289c6f485fdbf0a363949eb4deb1818cb1fbce55
Oscar <97081587@st.deltion.nl>
2023-09-12 10:37:24 +0200

    raadwoord

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -5,24 +5,25 @@ $rndmwoord = array_rand($array, 1);
 echo "Het woord is: ";
 //hier letters tellen van woord en dan in asterisks zetten
 $lengte = strlen ($array[$rndmwoord]);
-for ($i=0;$i<=$lengte;$i++) {
+$raadwoord = '';
+for ($i=1;$i<=$lengte;$i++) {
   echo "*";
 }
-$raadwoord = $rndmwoord;
+
 $teller=1;
 $begin=1;
 //hier dan de loop voor elk letter van de woord
 $letter = readline(" Kies een letter: ");
-  while ($rndmwoord != $raadwoord) {
-       if (($letter == $array[$rndmwoord])) {
-             echo "Het woord is: \n";
-          } elseif ($letter != $array[$rndmwoord]) {
+  while ($array[$rndmwoord] != $raadwoord) {
+       if (str_contains($array[$rndmwoord], $letter)) {
+             echo "Het woord is: $raadwoord \n";
+          } else {
              echo "letter $letter komt niet voor in het woord.\n";
           }
       $teller++;
     };
 
-echo "Het woord is: " .($array[$rndmwoord]); echo ", je hebt het geraden in $teller pogingen.\n";
+echo "Het woord is: " .$array[$rndmwoord]; echo ", je hebt het geraden in $teller pogingen.\n";
 echo "Wil je nog een keer spelen (ja/nee)";
 if ($letter == "ja") {
    echo "ja";
