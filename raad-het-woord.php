a48fc43e8e8872f479fc1892dd06fb6a58aec174
Oscar <97081587@st.deltion.nl>
2023-09-13 15:09:00 +0200

    uhm

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -1,5 +1,5 @@
 <?php
-$array = array("leerling","cirkel","eend","veiligheidsspeld","taart","arend","verkoper","dier","goed","ballonnen","borstel","pen","koffie","knie","kast");
+$array = array("aa");
 $rndmwoord = array_rand($array, 1);
 $raadwoord = '';
 
@@ -7,7 +7,7 @@ echo "Het woord is: ";
 //hier letters tellen van woord en dan in asterisks zetten
 $lengte = strlen ($array[$rndmwoord]);
 for ($i=1;$i<=$lengte;$i++) {
-  echo "$raadwoord";
+  echo "*";
 }
 
 $teller=1;
@@ -16,17 +16,16 @@ $begin=1;
 $letter = readline(" Kies een letter: ");
   while ($array[$rndmwoord] != $raadwoord) {
        if (str_contains($array[$rndmwoord], $letter)) {
-             echo "Het woord is: $raadwoord \n";
-          } else {
+          echo "Het woord is: $raadwoord \n";
+         } else {
              echo "letter $letter komt niet voor in het woord.\n";
           }
       $teller++;
     };
 
-echo "Het woord is: " .$array[$rndmwoord]; echo ", je hebt het geraden in $teller pogingen.\n";
-echo "Wil je nog een keer spelen (ja/nee)";
+echo "Het woord is: " .$array[$rndmwoord]; echo ", je hebt het geraden in $teller pogingen.\n Wil je nog een keer spelen (ja/nee)";
 if ($letter == "ja") {
-   echo "ja";
+   return (" ");
 } elseif ($letter == "nee") {  
    exit("zie je de volgende keer!");   
 }
