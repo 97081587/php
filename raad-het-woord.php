69fdda9e356a38ad2a8c8f172de6bb23312e93bb
Oscar <97081587@st.deltion.nl>
2023-10-05 11:36:11 +0200

    u

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -18,17 +18,18 @@ $teller=0;
 //hier dan de loop voor elk letter van de woord
  while ($raad_woord != $array_woorden[$rndmwoord]) {
    $raad_letter = readline(" Kies een letter: ");
-   $teller++;
-      if (str_contains($array_woorden[$rndmwoord], $raad_letter) == false) {
-         echo " letter $raad_letter komt niet voor in het woord.\n";
-      } elseif (str_contains($array_woorden[$rndmwoord], $raad_letter) == true) {
-         foreach (str_split($raad_woord) as $char) {
+   $teller++; 
+      if (str_contains($array_woorden[$rndmwoord], $raad_letter) == true) {
+        foreach (str_split($raad_woord) as $char) {
            $positie = strpos($array_woorden[$rndmwoord], $raad_letter,0);
            $raad_woord  = substr_replace($array_woorden[$rndmwoord],$raad_letter,$positie,1);
            echo $raad_woord;
-         };
-      };
- };
+           echo " 3 keer ";
+         }
+      } elseif (str_contains($array_woorden[$rndmwoord], $raad_letter) == false) { 
+         echo " letter $raad_letter komt niet voor in het woord.\n";
+      }
+ }
 
 echo "\n Het woord is: " .$array_woorden[$rndmwoord]; echo ", je hebt het geraden in $teller pogingen.\n Wil je nog een keer spelen (ja/nee)";
  if ($raad_letter == "nee") {  
