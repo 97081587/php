63927b0f25d0088f70a40eb5366992adaf7eaa27
Oscar <97081587@st.deltion.nl>
2023-10-05 14:27:13 +0200

    met hulp van chatgpt

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -19,16 +19,16 @@ $teller=0;
  while ($raad_woord != $array_woorden[$rndmwoord]) {
    $raad_letter = readline(" Kies een letter: ");
    $teller++; 
-      if (str_contains($array_woorden[$rndmwoord], $raad_letter) == true) {
-        foreach (str_split($raad_woord) as $char) {
-           $positie = strpos($array_woorden[$rndmwoord], $raad_letter,0);
-           $raad_woord  = substr_replace($array_woorden[$rndmwoord],$raad_letter,$positie,1);
-           echo $raad_woord;
-           echo " 3 keer ";
-         }
-      } elseif (str_contains($array_woorden[$rndmwoord], $raad_letter) == false) { 
-         echo " letter $raad_letter komt niet voor in het woord.\n";
+
+   $raad_word_array = str_split($raad_woord);
+
+   foreach (str_split($array_woorden[$rndmwoord]) as $position => $letter) {
+      if ($letter === $raad_letter) {
+          $raad_word_array[$position] = $raad_letter;
       }
+   }
+  $raad_woord = implode('', $raad_word_array);
+  echo $raad_woord;
  }
 
 echo "\n Het woord is: " .$array_woorden[$rndmwoord]; echo ", je hebt het geraden in $teller pogingen.\n Wil je nog een keer spelen (ja/nee)";
