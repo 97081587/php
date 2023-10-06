98458b598c6f1e16311befee799f235be475abba
Oscar <97081587@st.deltion.nl>
2023-10-06 09:57:38 +0200

    klaar (geen opnieuw functie)

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -15,29 +15,28 @@ for ($i=1;$i<=$lengte;$i++) {
 }
 $teller=0;
 
-//hier dan de loop voor elk letter van de woord
-
- while ($raad_woord != $array_woorden[$rndmwoord]) {
+//hier elk letter checken
+while ($raad_woord != $array_woorden[$rndmwoord]) {
    echo "\n";
    $raad_letter = readline(" Kies een letter: ");
+   $raad_letter = strtolower($raad_letter);
    $teller++;
    if (str_contains($array_woorden[$rndmwoord], $raad_letter)) {
-
       $raad_word_array = str_split($raad_woord);
-      foreach (str_split($array_woorden[$rndmwoord]) as $position => $letter) {
-         if ($letter === $raad_letter) {
-            $raad_word_array[$position] = $raad_letter;
+         foreach (str_split($array_woorden[$rndmwoord]) as $position => $letter) {
+            if ($letter === $raad_letter) {
+               $raad_word_array[$position] = $raad_letter;
+            }
          }
-      }
       $raad_woord = implode('', $raad_word_array);
-
       echo $raad_woord;
    } elseif (str_contains($array_woorden[$rndmwoord], $raad_letter) == false) { 
       echo " letter $raad_letter komt niet voor in het woord.";
    }
- }
-echo "\n Het woord is: " .$array_woorden[$rndmwoord]; echo ", je hebt het geraden in $teller pogingen.\n Wil je nog een keer spelen (ja/nee)";
- if ($raad_letter == "nee") {  
-   exit("zie je de volgende keer!");   
- }
+}
+echo "\n Het woord is " .$array_woorden[$rndmwoord]; echo ", je hebt het geraden in $teller pogingen. \n";
+$opnieuw = readline ("Wil je nog een keer spelen (ja/nee): ");
+if (strtolower($opnieuw) !== "ja") {  
+   exit(" ");   
+}
 ?>
