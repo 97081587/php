6003573ff736e9ab5ecae3ccf9bb44c4941e2a23
Oscar <97081587@st.deltion.nl>
2023-10-06 09:14:05 +0200

    klaar

--- raad-het-woord.php
+++ raad-het-woord.php
@@ -1,5 +1,5 @@
 <?php
-$array_woorden = array("abc","def");
+$array_woorden = array("vrede","herinneren","combinatie","membraan","tap","stadion,","montuur","zandloper","geeuw","aankondigingen","applaus","vouwen","mail","garnering","schaaldier");
 $rndmwoord = array_rand($array_woorden, 1);
 $raad_letter = '';
 $asterisk = '*';
@@ -16,21 +16,26 @@ for ($i=1;$i<=$lengte;$i++) {
 $teller=0;
 
 //hier dan de loop voor elk letter van de woord
+
  while ($raad_woord != $array_woorden[$rndmwoord]) {
+   echo "\n";
    $raad_letter = readline(" Kies een letter: ");
-   $teller++; 
+   $teller++;
+   if (str_contains($array_woorden[$rndmwoord], $raad_letter)) {
 
-   $raad_word_array = str_split($raad_woord);
-
-   foreach (str_split($array_woorden[$rndmwoord]) as $position => $letter) {
-      if ($letter === $raad_letter) {
-          $raad_word_array[$position] = $raad_letter;
+      $raad_word_array = str_split($raad_woord);
+      foreach (str_split($array_woorden[$rndmwoord]) as $position => $letter) {
+         if ($letter === $raad_letter) {
+            $raad_word_array[$position] = $raad_letter;
+         }
       }
-   }
-  $raad_woord = implode('', $raad_word_array);
-  echo $raad_woord;
- }
+      $raad_woord = implode('', $raad_word_array);
 
+      echo $raad_woord;
+   } elseif (str_contains($array_woorden[$rndmwoord], $raad_letter) == false) { 
+      echo " letter $raad_letter komt niet voor in het woord.";
+   }
+ }
 echo "\n Het woord is: " .$array_woorden[$rndmwoord]; echo ", je hebt het geraden in $teller pogingen.\n Wil je nog een keer spelen (ja/nee)";
  if ($raad_letter == "nee") {  
    exit("zie je de volgende keer!");   
