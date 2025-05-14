618efbee811537c4a3688d8728ed414105008657
Oscar <97081587@st.deltion.nl>
2025-05-14 10:43:05 +0200

    clean

--- kleinste-getal.php
+++ kleinste-getal.php
@@ -1,15 +1,4 @@
 <?php
-    // 100 tot 1
-    // $vergelijknummer = array("100", "99", "98", "97", "96", "95", "94", "93", "92", "91",
-    // "90", "89", "88", "87", "86", "85", "84", "83", "82", "81",
-    // "80", "79", "78", "77", "76", "75", "74", "73", "72", "71",
-    // "70", "69", "68", "67", "66", "65", "64", "63", "62", "61",
-    // "60", "59", "58", "57", "56", "55", "54", "53", "52", "51",
-    // "50", "49", "48", "47", "46", "45", "44", "43", "42", "41",
-    // "40", "39", "38", "37", "36", "35", "34", "33", "32", "31",
-    // "30", "29", "28", "27", "26", "25", "24", "23", "22", "21",
-    // "20", "19", "18", "17", "16", "15", "14", "13", "12", "11",
-    // "10", "9", "8", "7", "6", "5", "4", "3", "2", "1");
 
     // 100 willekeurige nummers
     $vergelijknummer = array("774", "581", "117", "668", "692", "616", "915", "366", "194", "708",
@@ -35,20 +24,18 @@
         // echo "$vergelijknummer[$i] test \n";
 
         //vergelijken met de hoofdnummer
-        //alle kleine getallen moeten naar de 1000 gaan en waarvan de meeste getallen zijn toegevoegd, is het kleinste getal.
+            //alle kleine getallen moeten naar de 1000 gaan en waarvan de meeste getallen zijn toegevoegd, is het kleinste getal.
             if ($vergelijknummer[$i] < $hoofdnummer) {
-                // echo "$vergelijknummer[$i] \n";
                 $RestNummer = 1000 - $vergelijknummer[$i];
-                echo "$RestNummer \n";
+                // echo "$RestNummer \n";
+                if ($RestNummer == 1 ) {
+                 // echo "$hoofdnummer Is het kleinste getal";
+                }              
             } 
             // voor als de hoofdnummer kleiner is dan het vergelijknummer, deze zijn niet de kleinste getallen.
             else if ($vergelijknummer[$i] > $hoofdnummer) {
                 // echo "Dit is geen kleine getal. \n";
             };
-
-        // if($hoofdnummer === $vergelijknummer[$i] ) {
-        //     echo "$hoofdnummer Is het kleinste getal";
-        // };
     };
     
 ?>
