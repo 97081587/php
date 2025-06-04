81621d2917fc4ce93e7052f019350eb206e28c1a
Oscar <97081587@st.deltion.nl>
2025-06-04 10:57:42 +0200

    eerste regel

--- kleinste-getal.php
+++ kleinste-getal.php
@@ -1,12 +1,17 @@
 <?php
 
-$VergelijkNummer = file_get_contents('kleinste-getal.txt');
-// echo $VergelijkNummer; 
+// $VergelijkNummer = file_get_contents('kleinste-getal.txt');
+
+$VergelijkNummer = fopen('kleinste-getal.txt', 'r');
+$EersteRegel = fgets($VergelijkNummer);
+fclose($VergelijkNummer);
+
+echo $EersteRegel; 
 
 $HoofdNummer = 0;
 
-if ($HoofdNummer < $VergelijkNummer) {
-    $HoofdNummer = $VergelijkNummer;
+if ($HoofdNummer < $EersteRegel) {
+    $HoofdNummer = $EersteRegel;
     echo $HoofdNummer;
 }
 
