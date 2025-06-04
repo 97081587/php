47f5f0e79ac31d4c48fbb4f550750858666e098d
Oscar <97081587@st.deltion.nl>
2025-06-04 09:43:03 +0200

    basis vergelijking

--- kleinste-getal.php
+++ kleinste-getal.php
@@ -1,5 +1,13 @@
 <?php
 
+$VergelijkNummer = file_get_contents('kleinste-getal.txt');
+// echo $VergelijkNummer; 
 
-    
+$HoofdNummer = 0;
+
+if ($HoofdNummer < $VergelijkNummer) {
+    $HoofdNummer = $VergelijkNummer;
+    echo $HoofdNummer;
+}
+
 ?>
