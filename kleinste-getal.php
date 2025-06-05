d66189c2fa9443c81dc4f3c5fa683d625f7df5cb
Oscar <97081587@st.deltion.nl>
2025-06-05 11:53:13 +0200

    code Chgpt

--- kleinste-getal.php
+++ kleinste-getal.php
@@ -1,18 +1,28 @@
 <?php
 $VergelijkNummer = fopen('kleinste-getal.txt', 'r');
-$HoofdNummer = " ";
+// $HoofdNummer = " ";
+$regels = [];
 
 //stopt bij de honderd en end of file
 for ($i=0;$i<100 && !feof($VergelijkNummer);$i++) {
     $Regel = fgets($VergelijkNummer);
     // echo $Regel; 
 
-    if ($HoofdNummer < $Regel) {
-        $HoofdNummer = $Regel;
-        echo $HoofdNummer;
-    } 
+    // if ($HoofdNummer < $Regel) {
+    //     $HoofdNummer = $Regel;
+    //     echo $HoofdNummer;
+    // } 
+    if ($Regel !== false) {
+        $regels[] = $Regel;
+    }
 }
 
 fclose($VergelijkNummer);
+
+echo $Regel;
+
+// foreach ($regels as $Regel) {
+//     echo $Regel;
+// }
 
 ?>
