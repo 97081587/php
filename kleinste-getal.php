a9ccb34aead7cdfd1985948b87f61b49972b4ae8
Oscar <97081587@st.deltion.nl>
2025-06-06 11:08:31 +0200

    hoofdnummer

--- kleinste-getal.php
+++ kleinste-getal.php
@@ -1,28 +1,28 @@
 <?php
 $VergelijkNummer = fopen('kleinste-getal.txt', 'r');
-// $HoofdNummer = " ";
 $regels = [];
+$HoofdNummer = " ";
 
 //stopt bij de honderd en end of file
 for ($i=0;$i<100 && !feof($VergelijkNummer);$i++) {
     $Regel = fgets($VergelijkNummer);
     // echo $Regel; 
 
-    // if ($HoofdNummer < $Regel) {
-    //     $HoofdNummer = $Regel;
-    //     echo $HoofdNummer;
-    // } 
-    if ($Regel !== false) {
+    // if ($Regel !== false) {
         $regels[] = $Regel;
-    }
+    // }
 }
 
 fclose($VergelijkNummer);
 
-echo $Regel;
+// echo $Regel;
 
-// foreach ($regels as $Regel) {
-//     echo $Regel;
-// }
+foreach ($regels as $Regel) {
+        if ($HoofdNummer < $Regel) {
+        $HoofdNummer = $Regel;
+        echo $HoofdNummer;
+    } 
+    // echo $Regel;
+}
 
 ?>
