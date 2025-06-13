9775373d6326784cc2bb07706670d43ff5492389
Oscar <97081587@st.deltion.nl>
2025-06-13 11:37:19 +0200

    Refactor kleinste-getal.php and kleinste-getal.txt for improved readability and formatting

--- kleinste-getal.php
+++ kleinste-getal.php
@@ -4,29 +4,29 @@ $regels = [];
 $HoofdNummer = '';
 
 //stopt bij de honderd en end of file
-// for ($i=0;$i<100 && !feof($VergelijkNummer);$i++) {
+for ($i=0;$i<100 && !feof($VergelijkNummer);$i++) {
     $Regel = fgets($VergelijkNummer);
-    // echo $Regel; 
+    echo $Regel; 
 
     //checkt of de regel niet leeg is
     if ($Regel !== false) {
         //van alle regels naar een regel
         $regels[] = $Regel;
     }
-// }
+}
 
 fclose($VergelijkNummer); 
 
 // foreach ($regels as $Regel) {
     // echo $Regel;
-    if ($HoofdNummer < $Regel) {
-        $HoofdNummer = $Regel;
-        echo $HoofdNummer;
-    }
-    elseif ($HoofdNummer > $Regel) {
-        $HoofdNummer != $Regel;
-        echo $HoofdNummer;
-    } 
+    // if ($HoofdNummer < $Regel) {
+    //     $HoofdNummer = $Regel;
+    //     echo $HoofdNummer;
+    // }
+    // elseif ($HoofdNummer > $Regel) {
+    //     $HoofdNummer != $Regel;
+    //     echo $HoofdNummer;
+    // } 
     // echo $Regel;
 // }
 
