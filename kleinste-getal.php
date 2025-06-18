33a178f17fad786ddc1082c0d233feaffb3bc383
Oscar <97081587@st.deltion.nl>
2025-06-18 11:48:30 +0200

    Update kleinste-getal.php to read from kleinste-getal10.txt and adjust loop conditions

--- kleinste-getal.php
+++ kleinste-getal.php
@@ -1,18 +1,31 @@
 <?php
-$VergelijkNummer = fopen('kleinste-getal.txt', 'r');
+$VergelijkNummer = fopen('kleinste-getal10.txt', 'r');
 $regels = [];
-$HoofdNummer = '';
+$HoofdNummer = "";
 
-//stopt bij de honderd en end of file
-for ($i=0;$i<100 && !feof($VergelijkNummer);$i++) {
+//stopt bij de honderd
+for ($i=0;$i<10;$i++) {
     $Regel = fgets($VergelijkNummer);
-    echo $Regel; 
+    echo $Regel;
+    if($HoofdNummer === "") {
+        $HoofdNummer = $Regel; // Zet de eerste regel als hoofdnummer
+    }
 
     //checkt of de regel niet leeg is
     if ($Regel !== false) {
         //van alle regels naar een regel
         $regels[] = $Regel;
     }
+
+
+    if ($HoofdNummer <= $Regel) {
+        $HoofdNummer === $Regel;
+        // echo $HoofdNummer;
+        // $i++;
+    } elseif ($HoofdNummer >= $Regel) {
+        // echo $HoofdNummer;
+        //   $i++;
+    } 
 }
 
 fclose($VergelijkNummer); 
@@ -21,11 +34,10 @@ fclose($VergelijkNummer);
     // echo $Regel;
     // if ($HoofdNummer < $Regel) {
     //     $HoofdNummer = $Regel;
-    //     echo $HoofdNummer;
-    // }
-    // elseif ($HoofdNummer > $Regel) {
-    //     $HoofdNummer != $Regel;
-    //     echo $HoofdNummer;
+        // echo $HoofdNummer;
+        // echo $Regel;
+    // } elseif ($HoofdNummer > $Regel) {
+    //       $i++;
     // } 
     // echo $Regel;
 // }
