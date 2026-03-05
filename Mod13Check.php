7d9de17ef62f16fee7090c599c51217b49bb39a1
Oscar <97081587@st.deltion.nl>
2026-03-05 08:49:04 +0100

    Enhance comments in Mod13Check.php for clarity on customer number validation logic

--- Mod13Check.php
+++ Mod13Check.php
@@ -2,14 +2,17 @@
 
 $klantNummer = readline("Voer een klantnummer in: ");
 
+// skipt de 0 / vervangt de 0 met een lege string
 $zonderNullen = str_replace('0', '', $klantNummer);
 
 $totaal = 0;
 
 foreach (str_split($zonderNullen) as $cijfer) {
+    // alle losse cijfers optellen
     $totaal += (int)$cijfer;
 }
 
+// mod 13 check
 if ($totaal % 13 === 0) {
     echo "Het klantnummer is geldig.\n";
 } else {
