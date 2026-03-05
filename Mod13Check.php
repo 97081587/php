7c31ecceb3a82c05865ce9ca83e34a260ead8af8
Oscar <97081587@st.deltion.nl>
2026-03-05 11:43:40 +0100

    Refactor Mod13Check.php to clean up commented code and improve readability

--- Mod13Check.php
+++ Mod13Check.php
@@ -1,34 +1,32 @@
 <?php
-
-// $klantNummer = readline("Voer een klantnummer in: ");
 $array = str_split(readline("Voer een klantnummer in: "));
-
-// $array = str_split($klantNummer);
-// print_r($array);
 
 // skipt de 0 / vervangt de 0 met een lege string
 $zonderNullen = str_replace('0', '', $array);
 
 //maakt er een string van
 $ZonderNullenString = implode('', $zonderNullen);
-
 echo "Zonder nullen: " . $ZonderNullenString . "\n";
+
+// print_r($zonderNullen) . "\n";
+
+// $NulAlsStop = str_replace('', '0', $ZonderNullenString);
+// echo "Nul als stop: " . $NulAlsStop . "\n";
 
 $totaal = 0;
 
-// while ($array) {
 
-foreach (str_split($ZonderNullenString) as $cijfer) {
-    // alle losse cijfers optellen en in totaal zetten
-    $totaal += (int)$cijfer;
-}
 
-// mod 13 check
-if ($totaal % 13 === 0) {
-    echo "Het klantnummer is geldig.\n";
-} else {
-    echo "Het klantnummer is ongeldig.\n";
-}
 
-// }
+    foreach (str_split($ZonderNullenString) as $cijfer) {
+        // alle losse cijfers optellen en in totaal zetten
+        $totaal += (int)$cijfer;
+    }
+
+    // mod 13 check
+    if ($totaal % 13 === 0) {
+        echo "Het klantnummer is geldig.\n";
+    } else {
+        echo "Het klantnummer is ongeldig.\n";
+    }
 ?>
