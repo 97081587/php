d4f4c91e61f0c59b40328166ac208c03a633ca8a
Oscar <97081587@st.deltion.nl>
2026-03-05 15:11:39 +0100

    Refactor Mod13Check.php to improve customer number generation logic and enhance readability

--- Mod13Check.php
+++ Mod13Check.php
@@ -4,23 +4,17 @@ $prefix = '2401160930';
 
 //10 klantnummers genereren
 for ($i = 0; $i < 10; $i++) {
-    $randEndNr = rand(1000, 9999);
-
-    // hele klantnummer combo
-    $klantnummer= $prefix . $randEndNr;
-
-    echo "Het gegenereerde klantnummer is: " . $klantnummer . "\n";
 
     //   mod 13 check
-    while ($klantnummer % 13 == 0){       
-        
-    }   
+    do {
+        // willekeurige 4 cijferige nummer genereren
+        $randEndNr = rand(1000, 9999);
 
-        // if ($klantnummer % 13 === 0) {
-        //     echo "Het klantnummer " . $klantnummer . " is geldig.\n";
-        // } elseif ($klantnummer % 13 !== 0) {
-        //     echo "Het klantnummer " . $klantnummer . " is ongeldig.\n";
+        // hele klantnummer combo
+        $klantnummer= $prefix . $randEndNr;
 
-        // } 
+    } while ($klantnummer % 13 !== 0);
+
+    echo "Het gegenereerde geldige klantnummer is: " . $klantnummer . "\n";
 }
 ?>  
