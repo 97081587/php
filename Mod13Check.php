10ea3efd2808c593afaaaa6ed923c951bd9a053f
Oscar <97081587@st.deltion.nl>
2026-03-05 10:42:44 +0100

    Refactor Mod13Check.php to replace customer number input handling and streamline zero removal logic

--- Mod13Check.php
+++ Mod13Check.php
@@ -1,13 +1,24 @@
 <?php
 
-$klantNummer = readline("Voer een klantnummer in: ");
+// $klantNummer = readline("Voer een klantnummer in: ");
+$array = str_split(readline("Voer een klantnummer in: "));
+
+// $array = str_split($klantNummer);
+// print_r($array);
 
 // skipt de 0 / vervangt de 0 met een lege string
-$zonderNullen = str_replace('0', '', $klantNummer);
+$zonderNullen = str_replace('0', '', $array);
+
+//maakt er een string van
+$ZonderNullenString = implode('', $zonderNullen);
+
+echo "Zonder nullen: " . $ZonderNullenString . "\n";
 
 $totaal = 0;
 
-foreach (str_split($zonderNullen) as $cijfer) {
+// while ($array) {
+
+foreach (str_split($ZonderNullenString) as $cijfer) {
     // alle losse cijfers optellen en in totaal zetten
     $totaal += (int)$cijfer;
 }
@@ -19,4 +30,5 @@ if ($totaal % 13 === 0) {
     echo "Het klantnummer is ongeldig.\n";
 }
 
+// }
 ?>
