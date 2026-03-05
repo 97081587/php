694de216149a56c5bfc88fce7fb1fd69c6e58f83
Oscar <97081587@st.deltion.nl>
2026-03-05 15:21:52 +0100

    Remove unnecessary whitespace in Mod13Check.php for improved readability

--- Mod13Check.php
+++ Mod13Check.php
@@ -1,20 +1,15 @@
 <?php
 //prefix
 $prefix = '2401160930';
-
 //10 klantnummers genereren
 for ($i = 0; $i < 10; $i++) {
-
     // loopt tot mod 13 is 0
     do {
         // willekeurige 4 cijferige nummer genereren
         $randEndNr = rand(1000, 9999);
-
         // hele klantnummer combo
         $klantnummer= $prefix . $randEndNr;
-
     } while ($klantnummer % 13 !== 0);
-
     echo "Het gegenereerde geldige klantnummer is: " . $klantnummer . "\n";
 }
 ?>  
